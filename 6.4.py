91972ada716e41755258f9ffde51ff2ec49a2539
sallymcclung <75751885+sallymcclung@users.noreply.github.com>
2020-12-17 13:59:47 -0600

    Create 6.4.py

---
+++ 6.4.py
@@ -0,0 +1,13 @@
+"""
+Exercise 6.4: There is a string method called count that is similar to the function
+in the previous exercise. Read the documentation of this method at:
+
+https://docs.python.org/library/stdtypes.html#string-methods
+
+Write an invocation that counts the number of times the letter a occurs in 'banana'.
+"""
+string = "banana"
+substring = "a"
+
+subcount = string.count(substring)
+print(subcount)
