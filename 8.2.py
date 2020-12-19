c75d5b3ca3503ed6ae712bdf1968a68ba9049e9c
sallymcclung <75751885+sallymcclung@users.noreply.github.com>
2020-12-19 17:07:50 -0600

    Create 8.2.py

---
+++ 8.2.py
@@ -0,0 +1,17 @@
+"""
+Exercise 8.2: Figure out which line of the above program is still not properly
+guarded. See if you can construct a text file which causes the program to fail
+and then modify the program so that the line is properly guarded and test it
+to make sure it handles your new text file.
+"""
+
+fhand = open("8.2.txt")
+
+for line in fhand:
+    words = line.split()
+    #print("Debug:", words)
+    if len(words) < 3:
+        continue
+    if words[0] != "From":
+        continue
+    print(words[2])
